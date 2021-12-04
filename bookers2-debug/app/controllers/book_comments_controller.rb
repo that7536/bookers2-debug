c3e0b59c5ab16d38e9cedc35739bfe8501181eb0
EC2 Default User <ec2-user@ip-172-31-84-169.ec2.internal>
2021-12-04 06:56:20 +0000

    task3

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -1,12 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    comment.book_id = @book.id
+    if comment.save
+      redirect_to book_path(@book)
+    else
+      @book_new = Book.new
+      @user = @book.user
+      @book_comment = BookComment.new
+      render 'books/show'
+    end
   end
 
   def destroy
