1d1a1582dcc6992060517d19a3d1dfafbe2a9993
EC2 Default User <ec2-user@ip-172-31-84-169.ec2.internal>
2021-12-04 05:24:50 +0000

    task3

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @books = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
